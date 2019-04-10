30218040e82fd6e3d20ad9e9f95ff3ae2d7e16ec
irmerk <jolenelanglinais@gmail.com>
2019-04-10 18:30:14 -0400

    fix(webpack): configure webpack loaders for import
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -11,21 +11,43 @@ module.exports = {
     },
     module: {
         rules: [
-          {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader"
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[ext]",
+                        outputPath: "fonts/"
+                    }
+                }]
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+                include: [
+                    path.join(__dirname, 'src'),
+                    /node_modules/
+                ],
+            },
+            {
+                test: /\.(jpg|png|gif|jpeg|woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader?limit=100000'
             }
-          },
-          {
-              test: /\.html$/,
-              use: [
-                  {
-                      loader: "html-loader"
-                  }
-              ]
-          }
         ]
     },
     plugins: [
