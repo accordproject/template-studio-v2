e0fe110281d1dbe3211f856d39396d5f7d3f74ec
irmerk <jolenelanglinais@gmail.com>
2019-04-10 16:31:49 -0400

    fix(*): remove dist from git
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-      filename: 'main.js',
+      filename: 'bundle.js',
       path: path.resolve(__dirname, 'dist')
     },
     module: {
