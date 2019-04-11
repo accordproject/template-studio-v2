5daa866ee0d2bd7b782b8b45d49d2b99bd1f387a
irmerk <jolenelanglinais@gmail.com>
2019-04-11 18:03:07 -0400

    feat(src): prepare for redux
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import { createStore, applyMiddleware } from 'redux';
+// import { Provider } from 'react-redux';
 
 import TemplateStudio from './TemplateStudio';
+// import reducer from './store/reducer';
+
+// const store = createStore(reducer);
+// const action = type => store.dispatch({type})
 
 const wrapper = document.getElementById('root');
 wrapper ? ReactDOM.render(<TemplateStudio />, wrapper) : false;
+
+{/* <Provider store={store}><TemplateStudio /></Provider> */}
