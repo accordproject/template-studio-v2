d4d2fa4d4fe8eb9240777459da1a4549acc51860
Diana Lease <dianarlease@gmail.com>
2019-04-19 12:46:50 -0400

    feat(redux-saga): put templates in store and render templates from store
    Signed-off-by: Diana Lease <dianarlease@gmail.com>

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,35 +1,18 @@
 /* Saga */
-// import { delay } from "redux-saga";
 import { all, takeLatest, put } from "redux-saga/effects";
 
 
 
-export function* pushTemplatesToStore(templates) {
-    console.log('Saga here: ', templates)
-    yield put({ type: 'AP_TEMPLATES_RECEIVED', templates });
-
-
-
-
-
-
-
-
-
-
-    // const json = yield fetch('https://newsapi.org/v1/articles?source=cnn&apiKey=c39a26d9c12f48dba2a5c00e35684ecc')
-    //   .then(response => response.json(), );
-  
-    // yield put({ type: 'AP_TEMPLATES_RECEIVED', json: json.articles, });
+export function* pushTemplatesToStore(action) {
+    yield put({ type: 'AP_TEMPLATES_RECEIVED', templates: action.templates });
 }
 
 
 function* actionWatcher() {
-    yield takeLatest('AP_TEMPLATES_RECEIVED', pushTemplatesToStore)
+    yield takeLatest('GET_AP_TEMPLATES', pushTemplatesToStore)
 }
 
 
-
 export default function* rootSaga() {
-    yield pushTemplatesToStore();
+    yield all([actionWatcher()]);
 }
