74e728465951d6f8b79476a8314c32801f81ac88
irmerk <jolenelanglinais@gmail.com>
2019-04-19 09:14:26 -0400

    feat(store): initiate redux reducer
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,20 +1,16 @@
 const initialState = {
-    amount:20
+    templatesAP: []
 };
 
-const reducer = (state=initialState, action) => {
-    const newState = {...state};
+const AP_TEMPLATES_RECEIVED = 'AP_TEMPLATES_RECEIVED';
 
+const reducer = (state = initialState, action) => {
     switch(action.type){
-        case 'ACTION_ONE': 
-            newState.amount += action.value;
-            break;
-        
-        case 'ACTION_TWO': 
-            newState.amount -= action.value;
-            break;
+        case AP_TEMPLATES_RECEIVED:
+            return {...state, templatesAP: action.templates};
+        default:
+            return state;
     }
-    return newState;
 };
 
 export default reducer;
