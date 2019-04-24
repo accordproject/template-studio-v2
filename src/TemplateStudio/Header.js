3da62f9047bd89d57f29e67abef8e24c0e810a65
irmerk <jolenelanglinais@gmail.com>
2019-04-24 11:53:50 -0400

    feat(TemplateStudio): initiate functional header component - NA
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

---
+++ src/TemplateStudio/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import image from '../../public/img/logo.png';
+
+const HeaderWrapper = styled.div`
+    display: flex;
+    height: 37px;
+    width: 100vw;
+    background-color: #1B1C1C;
+    color: white;
+`;
+
+const HeaderTitle = styled.div`
+    display: flex;
+    align-items: center;
+    margin-left: 30px;
+    padding-right: 20px;
+    border-right: 1px solid #2D2E2E;
+    text-align: center;
+    font-weight: bold;
+`;
+
+const HeaderImage = styled.img`
+    height: 27px;
+    margin-top: 5px;
+    margin-left: 15px;
+`;
+
+const Header = () => (
+    <HeaderWrapper>
+            <a href="https://www.accordproject.org">
+                <HeaderImage src={image} alt="Accord Project" />
+            </a>
+        <HeaderTitle>Template Studio</HeaderTitle>
+    </HeaderWrapper>
+);
+
+export default Header;
