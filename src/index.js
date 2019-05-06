5383200ba53cf22109dec241ed61badb695cdd1d
irmerk <jolenelanglinais@gmail.com>
2019-05-06 17:15:43 -0400

    fix(src): adjust import name for app - I4
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import App from './containers/App';
+import MainApp from './containers/App';
 import store from './store';
 
 const wrapper = document.getElementById('root');
-wrapper ? ReactDOM.render(<Provider store={store}><App /></Provider>, wrapper) : false;
+wrapper ? ReactDOM.render(<Provider store={store}><MainApp /></Provider>, wrapper) : false;
