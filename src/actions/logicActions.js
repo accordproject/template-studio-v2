74a4d5572dbe407eed6f633457250d9709ad1c09
Diana Lease <dianarlease@gmail.com>
2019-05-10 15:15:52 -0400

    refactor(logic&sample): refactor logic & sample sagas and actions
    Signed-off-by: Diana Lease <dianarlease@gmail.com>

--- src/actions/logicActions.js
+++ src/actions/logicActions.js
@@ -2,3 +2,8 @@ export const updateLogicMockAction = value => ({
   type: 'UPDATE_LOGIC_MOCK',
   logic: value,
 });
+
+export const updateLogicMockSuccess = logic => ({
+  type: 'UPDATE_LOGIC_MOCK_SUCCEEDED',
+  logic,
+});
