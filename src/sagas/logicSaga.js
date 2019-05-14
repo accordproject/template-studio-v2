401a585216b6284c0104a67b810e13cd3d47ac2d
irmerk <jolenelanglinais@gmail.com>
2019-05-14 16:16:22 -0400

    chore(sagas): include jsdoc - I-22
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- src/sagas/logicSaga.js
+++ src/sagas/logicSaga.js
@@ -1,6 +1,9 @@
 import { takeLatest, put } from 'redux-saga/effects';
 import * as actions from '../actions/logicActions';
 
+/**
+ * saga to take a logic action and update successfully in the store
+ */
 export function* updateLogicOnStore(logicMockAction) {
   yield put(actions.updateLogicMockSuccess(logicMockAction.logic));
 }
