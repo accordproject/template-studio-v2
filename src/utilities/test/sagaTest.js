5ccec66fb17c7bca735d8cf9c9421dccfe410621
irmerk <jolenelanglinais@gmail.com>
2019-05-14 16:17:03 -0400

    feat(utilities): include optional state for test utility - I-22
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- src/utilities/test/sagaTest.js
+++ src/utilities/test/sagaTest.js
@@ -1,10 +1,18 @@
 import { runSaga } from 'redux-saga';
 
-export async function recordSaga(saga, initialAction) {
+/**
+ * saga to start given saga outside middleware with an action
+ * --> the options object is used to define behavior of side effects
+ * --> dispatch here fulfils put effects
+ * --> adds dispatched actions to a list, then returns after saga is finished
+ */
+export async function recordSaga(saga, initialAction, state) {
   const dispatched = [];
-
   await runSaga(
-    { dispatch: (action) => { dispatched.push(action); } },
+    {
+      dispatch: action => dispatched.push(action),
+      getState: () => state,
+    },
     saga,
     initialAction,
   ).toPromise();
