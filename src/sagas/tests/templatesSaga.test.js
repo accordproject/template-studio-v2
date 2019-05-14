439edfe5974978c994a7e1630c9970b0adfdeb80
irmerk <jolenelanglinais@gmail.com>
2019-05-14 09:06:32 -0400

    test(sagas): implement tests for addnewtemplatetostore - I-22
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- src/sagas/tests/templatesSaga.test.js
+++ src/sagas/tests/templatesSaga.test.js
@@ -1,12 +1,8 @@
-import { pushTemplatesToStore } from '../templatesSaga';
+import { addNewTemplateToStore, pushTemplatesToStore } from '../templatesSaga';
 import { recordSaga } from '../../utilities/test/sagaTest';
-import { getTemplatesSuccess } from '../../actions/templatesActions';
+import { addNewTemplateSuccess, getTemplatesSuccess } from '../../actions/templatesActions';
 
 describe('pushTemplatesToStore', () => {
-  //   beforeEach(() => {
-  //     jest.resetAllMocks();
-  //   });
-
   it('should dispatch the action getTemplatesSuccess', async () => {
     const dispatched = await recordSaga(
       pushTemplatesToStore,
@@ -21,5 +17,23 @@ describe('pushTemplatesToStore', () => {
       getTemplatesSuccess,
     );
     expect(dispatched[0].templates[0].url).toContain('templates.accordproject.org');
+  });
+});
+
+describe('addNewTemplateToStore', () => {
+  it('should dispatch the action addNewTemplateSuccess', async () => {
+    const dispatched = await recordSaga(
+      addNewTemplateToStore,
+      addNewTemplateSuccess,
+    );
+    expect(dispatched[0].type).toEqual('ADD_NEW_TEMPLATE_SUCCEEDED');
+  });
+
+  it('should add a template with a stock name', async () => {
+    const dispatched = await recordSaga(
+      addNewTemplateToStore,
+      addNewTemplateSuccess,
+    );
+    expect(dispatched[0].template.name).toContain('Temporary New Template');
   });
 });
