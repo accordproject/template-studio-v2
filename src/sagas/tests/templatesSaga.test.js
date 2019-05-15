8cc82c639d6c981f8ee202b1be8483b8bfdaa713
irmerk <jolenelanglinais@gmail.com>
2019-05-15 16:45:44 -0400

    test(sagas): initiate test for failed templates fetch - I-22
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- src/sagas/tests/templatesSaga.test.js
+++ src/sagas/tests/templatesSaga.test.js
@@ -1,6 +1,8 @@
+import { call } from 'redux-saga/effects';
+import { expectSaga } from 'redux-saga-test-plan';
 import { addNewTemplateToStore, pushTemplatesToStore } from '../templatesSaga';
 import { recordSaga } from '../../utilities/test/sagaTest';
-import { addNewTemplateSuccess, getTemplatesSuccess } from '../../actions/templatesActions';
+import { getTemplatesError, addNewTemplateSuccess, getTemplatesSuccess } from '../../actions/templatesActions';
 
 describe('pushTemplatesToStore', () => {
   it('should dispatch the action getTemplatesSuccess', async () => {
@@ -36,4 +38,11 @@ describe('addNewTemplateToStore', () => {
     );
     expect(dispatched[0].template.name).toContain('Temporary New Template');
   });
+});
+
+describe('pushTemplatesToStore Error', () => {
+  it('should ERROR', async () => expectSaga(pushTemplatesToStore)
+    .provide([[call(pushTemplatesToStore), () => { throw new Error(); }]])
+    .put(getTemplatesError(() => { throw new Error(); }))
+    .run());
 });
