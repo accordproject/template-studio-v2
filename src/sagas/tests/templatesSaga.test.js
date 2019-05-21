18c869716e56d89fc0399971891319531e8d53b2
Diana Lease <dianarlease@gmail.com>
2019-05-21 11:07:20 -0400

    test(templatesSaga): update templatesSaga to use mocks and add error test case
    Signed-off-by: Diana Lease <dianarlease@gmail.com>

--- src/sagas/tests/templatesSaga.test.js
+++ src/sagas/tests/templatesSaga.test.js
@@ -1,22 +1,54 @@
+import { TemplateLibrary } from '@accordproject/cicero-core';
 import { addNewTemplateToStore, pushTemplatesToStore } from '../templatesSaga';
 import { recordSaga } from '../../utilities/test/sagaTest';
-import { addNewTemplateSuccess, getTemplatesSuccess } from '../../actions/templatesActions';
+
+const mockedTemplateIndex = [{
+  ciceroVersion: '^0.12.0',
+  description: 'This clause allows the receiver of goods to inspect them for a given time period after delivery.',
+  name: 'acceptance-of-delivery',
+  type: 1,
+  uri: 'ap://acceptance-of-delivery@0.11.0#311de48109cce10e6b2e33ef183ccce121886d0b76754d649d5054d1084f93cd',
+  url: 'https://templates.accordproject.org/archives/acceptance-of-delivery@0.11.0.cta',
+  version: '0.11.0',
+}];
+
+jest.mock('@accordproject/cicero-core', () => ({
+  TemplateLibrary: jest.fn(),
+}));
+
+beforeEach(() => {
+  jest.resetModules();
+});
 
 describe('pushTemplatesToStore', () => {
   it('should dispatch the action getTemplatesSuccess', async () => {
+    TemplateLibrary.mockImplementation(() => ({
+      getTemplateIndex: () => Promise.resolve(mockedTemplateIndex),
+    }));
     const dispatched = await recordSaga(
       pushTemplatesToStore,
-      getTemplatesSuccess,
     );
     expect(dispatched[0].type).toEqual('GET_AP_TEMPLATES_SUCEEDED');
   });
 
   it('should receive an array of templates from accordproject.org', async () => {
+    TemplateLibrary.mockImplementation(() => ({
+      getTemplateIndex: () => Promise.resolve(mockedTemplateIndex),
+    }));
     const dispatched = await recordSaga(
       pushTemplatesToStore,
-      getTemplatesSuccess,
     );
     expect(dispatched[0].templates[0].url).toContain('templates.accordproject.org');
+  });
+
+  it('should dispatch an error if templates fetch fails', async () => {
+    TemplateLibrary.mockImplementation(() => ({
+      getTemplateIndex: () => Promise.reject(new Error('Unable to recieve templates')),
+    }));
+    const dispatched = await recordSaga(
+      pushTemplatesToStore,
+    );
+    expect(dispatched[0].type).toContain('AP_TEMPLATES_ERROR');
   });
 });
 
@@ -24,7 +56,6 @@ describe('addNewTemplateToStore', () => {
   it('should dispatch the action addNewTemplateSuccess', async () => {
     const dispatched = await recordSaga(
       addNewTemplateToStore,
-      addNewTemplateSuccess,
     );
     expect(dispatched[0].type).toEqual('ADD_NEW_TEMPLATE_SUCCEEDED');
   });
@@ -32,7 +63,6 @@ describe('addNewTemplateToStore', () => {
   it('should add a template with a stock name', async () => {
     const dispatched = await recordSaga(
       addNewTemplateToStore,
-      addNewTemplateSuccess,
     );
     expect(dispatched[0].template.name).toContain('Temporary New Template');
   });
