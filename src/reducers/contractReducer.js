e0db1644b6d4dd6ddb35205d9fe424ec8a2d78cd
Diana Lease <dianarlease@gmail.com>
2019-06-19 09:23:49 -0400

    feat(contractReducer): add error to reducer
    Signed-off-by: Diana Lease <dianarlease@gmail.com>

--- src/reducers/contractReducer.js
+++ src/reducers/contractReducer.js
@@ -19,6 +19,7 @@ const initialState = {
       }]
     }
   }),
+  error: null,
 };
 
 const reducer = (state = initialState, action) => {
@@ -28,6 +29,11 @@ const reducer = (state = initialState, action) => {
         ...state,
         markdown: action.markdown,
         slateValue: action.slateValue,
+      };
+    case 'DOCUMENT_EDITED_ERROR':
+      return {
+        ...state,
+        error: action.error,
       };
     default:
       return state;
