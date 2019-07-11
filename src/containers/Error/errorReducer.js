3a4b39613b75aa48ff94f6098f9ace71f2413d48
irmerk <jolenelanglinais@gmail.com>
2019-07-11 16:31:13 -0400

    feat(Error): abstract our prop reduction logic - I61
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

---
+++ src/containers/Error/errorReducer.js
@@ -0,0 +1,12 @@
+import * as R from 'ramda';
+
+const modelErrorGather = state => R.toPairs(R.path(['modelState', 'error'], state) || {})
+  .map(([clauseTemplateId, modelError]) => ({ clauseTemplateId, modelError }));
+
+const parseErrorGather = state => R.toPairs(R.path(['contractState', 'clauses'], state) || {})
+  .map(([clauseId, clause]) => ({ clauseId, parseError: clause.parseError }))
+  .filter(({ parseError }) => !R.isNil(parseError));
+
+const errorsGenerator = state => [...modelErrorGather(state), ...parseErrorGather(state)];
+
+export default errorsGenerator;
--- src/containers/Error/index.js
+++ src/containers/Error/index.js
@@ -1,20 +1,15 @@
 import React from 'react';
-import * as R from 'ramda';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { ErrorLogger } from '@accordproject/cicero-ui';
 
+import errorsGenerator from './errorReducer';
+
 const ErrorWrapper = styled.div`
   width: 100%;
-  position: fixed;
-  // bottom: -360px;
-  bottom: 0;
-  transition: 1s;
-  &:hover {
-    bottom: 0;
-  }
 `;
+
 const ErrorContainer = props => (
   <ErrorWrapper>
     <ErrorLogger errors={props.errors}/>
@@ -26,13 +21,7 @@ ErrorContainer.propTypes = {
 };
 
 const mapStateToProps = state => ({
-  errors: {
-    modelErrors: R.toPairs(R.path(['modelState', 'error'], state) || {})
-      .map(([clauseTemplateId, modelError]) => ({ clauseTemplateId, modelError })),
-    parseErrors: R.toPairs(R.path(['contractState', 'clauses'], state) || {})
-      .map(([clauseId, clause]) => ({ clauseId, parseError: clause.parseError }))
-      .filter(({ parseError }) => !R.isNil(parseError)),
-  },
+  errors: errorsGenerator(state),
 });
 
 export default connect(mapStateToProps)(ErrorContainer);
