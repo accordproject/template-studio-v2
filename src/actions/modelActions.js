c92ed9b4d02a3d66b47e59a96137770414357360
irmerk <jolenelanglinais@gmail.com>
2019-07-16 15:51:20 -0400

    feat(src): introduce remove saga functionality for model error - I61
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- src/actions/modelActions.js
+++ src/actions/modelActions.js
@@ -1,5 +1,6 @@
 import {
-  UPDATE_MODEL_ERROR_SUCCEEDED,
+  UPDATE_MODEL_ADD_ERROR_SUCCEEDED,
+  UPDATE_MODEL_REMOVE_ERROR_SUCCEEDED,
   UPDATE_MODEL_MANAGER_SUCCEEDED,
   VALIDATE_CLAUSE_MODEL_FILES,
 } from './constants';
@@ -9,8 +10,14 @@ export const updateModelManagerSuccess = modelManager => ({
   modelManager,
 });
 
-export const updateModelManagerError = (error, clauseTemplateId) => ({
-  type: UPDATE_MODEL_ERROR_SUCCEEDED,
+export const updateModelManagerAddError = (error, clauseTemplateId) => ({
+  type: UPDATE_MODEL_ADD_ERROR_SUCCEEDED,
+  error,
+  clauseTemplateId
+});
+
+export const updateModelManagerRemoveError = (error, clauseTemplateId) => ({
+  type: UPDATE_MODEL_REMOVE_ERROR_SUCCEEDED,
   error,
   clauseTemplateId
 });
