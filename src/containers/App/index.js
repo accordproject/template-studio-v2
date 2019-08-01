331fc95c16e633e74b9ad0ffdf32cdac3aa41b1d
irmerk <jolenelanglinais@gmail.com>
2019-08-01 16:44:01 -0400

    feat(App): init logic for error navigation - I61
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -29,6 +29,43 @@ const ContentWrapper = styled.div`
   overflow-y: hidden;
 `;
 
+/* These changes are to navigate PARSE ERRORS only */
+/* Model errors will come later */
+
+// This should be passed to the ContractEditor (or current)
+// Adjust to find the node in the Slate DOM
+const findClauseNode = (clauseId) => {
+  const { value } = this.state;
+
+  let clauseNode = null;
+  value.document.nodes.forEach((n) => {
+    if (n.type !== 'clause') return;
+    if (n.data.get('clauseId') === clauseId) {
+      clauseNode = n;
+    }
+  });
+  return clauseNode;
+};
+
+// This should also be passed to the ContractEditor (or current)
+// Will scroll to the node in the Slate DOM
+const scrollToClause = (clauseNode) => {
+  const el = document.querySelector(`[data-key="${clauseNode.key}"]`);
+  el.scrollIntoView({ behavior: 'smooth' });
+};
+
+// This gets passed to error, when the file is clicked,
+// this runs with the error's associated clauseId
+const navigateToClauseError = (clauseId) => {
+  const clauseNode = findClauseNode(clauseId);
+
+  if (!clauseNode) {
+    console.error('Error: clause not found');
+    return;
+  }
+  scrollToClause(clauseNode);
+};
+
 export const App = () => (
     <AppWrapper>
       <ErrorModalComponent />
