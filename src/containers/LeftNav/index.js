0dfdd85b37c8d043a51ef195ee1f85672ebdcd6b
irmerk <jolenelanglinais@gmail.com>
2019-08-13 15:15:07 -0400

    feat(LeftNav): migrate logic to cicero ui - I55
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- src/containers/LeftNav/index.js
+++ src/containers/LeftNav/index.js
@@ -1,14 +1,14 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 
+import { Navigation } from '@accordproject/cicero-ui';
+
 import TextButton from '../../components/TextButton';
-import ClauseNav from './ClauseNav';
-import SubHeading from './SubHeadingBtn';
 import { setCurrentEditorAction } from '../../actions/appActions';
 
-import { AP_THEME, FILE_BAR, LEFT_NAV } from '../App/themeConstants';
+import { AP_THEME, FILE_BAR } from '../App/themeConstants';
 
 const LeftSidebar = styled.div`
   background-color: ${AP_THEME.DARK_BLUE};
@@ -18,7 +18,9 @@ const LeftSidebar = styled.div`
 const LeftNavWrapper = styled.div`
   padding: 15px;
   overflow-x: hidden;
+  overflow-y: hidden;
   background-color: ${AP_THEME.DARK_BLUE};
+  height: 100%;
 `;
 
 const FileOptions = styled.div`
@@ -53,22 +55,11 @@ const FileOptionButtons = styled.button`
 
 const NavWrapper = styled.div`
   padding-top: 10px;
-`;
-
-const Heading = styled.h2`
-  color: ${LEFT_NAV.HEADING};
-  font-size: 14px;
-  font-weight: 600;
-  line-height: 14px;
-  text-decoration: none;
-  text-align: left;
-  margin: 10px;
+  height: 100%;
 `;
 
 export const LeftNav = (props) => {
-  const { setCurrentEditor, slateValue, clauses } = props;
   const [navVisible, setNavVisible] = useState(true);
-  const [showExpandedClause, setShowExpandedClause] = useState({});
 
   const buttonRef = useRef(null);
 
@@ -77,11 +68,6 @@ export const LeftNav = (props) => {
     buttonRef.current.blur();
   };
 
-  const onClauseClick = useCallback((id) => {
-    setShowExpandedClause({ ...showExpandedClause, [id]: !showExpandedClause[id] });
-  }, [showExpandedClause]);
-
-  const clauseNodes = slateValue.toJSON().document.nodes.filter(node => node.type === 'clause');
   return (
     <LeftSidebar>
       <FileOptions>
@@ -97,30 +83,8 @@ export const LeftNav = (props) => {
       { navVisible ? '< Hide Navigation' : 'Show Navigation >'}
     </TextButton>
     <NavWrapper>
-      { navVisible && <React.Fragment>
-        <Heading>CONTRACT</Heading>
-        <SubHeading onClick={() => setCurrentEditor(null, 'contract')}>Contract</SubHeading>
-        <br />
-        <Heading>CLAUSES</Heading>
-        { Object.keys(clauses).length
-          ? clauseNodes.map((clauseNode) => {
-            // clauseid is the id of the clause instance
-            const { clauseid, src } = clauseNode.data.attributes;
-            if (!clauses[clauseid]) return null;
-            // clauseTemplateRef is the id of the clause template which the clause is an instance of
-            const { clauseTemplateRef } = clauses[clauseid];
-            return (
-              <ClauseNav
-                key={clauseid}
-                showExpandedClause={showExpandedClause}
-                onClauseClick={onClauseClick}
-                clauseTemplateId={clauseTemplateRef}
-                src={src}
-                setCurrentEditor={setCurrentEditor}
-              />);
-          }) : null
-        }
-      </React.Fragment> }
+
+    {navVisible && <Navigation headers={props.headers} />}
     </NavWrapper>
     </LeftNavWrapper>
   </LeftSidebar>
@@ -128,6 +92,7 @@ export const LeftNav = (props) => {
 };
 
 LeftNav.propTypes = {
+  headers: PropTypes.array,
   slateValue: PropTypes.object.isRequired,
   setCurrentEditor: PropTypes.func.isRequired,
   clauses: PropTypes.object,
@@ -136,6 +101,7 @@ LeftNav.propTypes = {
 const mapStateToProps = state => ({
   slateValue: state.contractState.slateValue,
   clauses: state.contractState.clauses,
+  headers: state.contractState.headers,
 });
 
 const mapDispatchToProps = dispatch => ({
