0fcbafa1f7861e0affb3cbb6f5502b40bc027960
irmerk <jolenelanglinais@gmail.com>
2019-08-15 15:36:39 -0400

    feat(LeftNav): swap new nav for hidden nav - I55
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- src/containers/LeftNav/index.js
+++ src/containers/LeftNav/index.js
@@ -1,14 +1,18 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 
 import { Navigation } from '@accordproject/cicero-ui';
 
+import { navigateToHeader } from '../../utilities/navigateClause';
+
 import TextButton from '../../components/TextButton';
+import ClauseNav from './ClauseNav';
+import SubHeading from './SubHeadingBtn';
 import { setCurrentEditorAction } from '../../actions/appActions';
 
-import { AP_THEME, FILE_BAR } from '../App/themeConstants';
+import { AP_THEME, FILE_BAR, LEFT_NAV } from '../App/themeConstants';
 
 const LeftSidebar = styled.div`
   background-color: ${AP_THEME.DARK_BLUE};
@@ -59,8 +63,20 @@ const NavWrapper = styled.div`
   // overflow-y: inherit;
 `;
 
+const Heading = styled.h2`
+  color: ${LEFT_NAV.HEADING};
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 14px;
+  text-decoration: none;
+  text-align: left;
+  margin: 10px;
+`;
+
 export const LeftNav = (props) => {
+  const { setCurrentEditor, slateValue, clauses } = props;
   const [navVisible, setNavVisible] = useState(true);
+  const [showExpandedClause, setShowExpandedClause] = useState({});
 
   const buttonRef = useRef(null);
 
@@ -71,10 +87,17 @@ export const LeftNav = (props) => {
 
   const navigationPropsInput = {
     headers: props.headers,
+    navigateHeader: navigateToHeader,
     navigationProps: {
       NAVIGATE_SWITCH_FILES_VISIBLE: false,
     }
   };
+
+  const onClauseClick = useCallback((id) => {
+    setShowExpandedClause({ ...showExpandedClause, [id]: !showExpandedClause[id] });
+  }, [showExpandedClause]);
+
+  const clauseNodes = slateValue.toJSON().document.nodes.filter(node => node.type === 'clause');
 
   return (
     <LeftSidebar>
@@ -88,11 +111,35 @@ export const LeftNav = (props) => {
       onClick={handleClick}
       display={'block'}
     >
-      { navVisible ? '< Hide Navigation' : 'Show Navigation >'}
+      { navVisible ? 'Clause Navigation' : 'Contract Navigation'}
     </TextButton>
     <NavWrapper>
 
     {navVisible && <Navigation {...navigationPropsInput} />}
+      { !navVisible && <React.Fragment>
+        <Heading>CONTRACT</Heading>
+        <SubHeading onClick={() => setCurrentEditor(null, 'contract')}>Contract</SubHeading>
+        <br />
+        <Heading>CLAUSES</Heading>
+        { Object.keys(clauses).length
+          ? clauseNodes.map((clauseNode) => {
+            // clauseid is the id of the clause instance
+            const { clauseid, src } = clauseNode.data.attributes;
+            if (!clauses[clauseid]) return null;
+            // clauseTemplateRef is the id of the clause template which the clause is an instance of
+            const { clauseTemplateRef } = clauses[clauseid];
+            return (
+              <ClauseNav
+                key={clauseid}
+                showExpandedClause={showExpandedClause}
+                onClauseClick={onClauseClick}
+                clauseTemplateId={clauseTemplateRef}
+                src={src}
+                setCurrentEditor={setCurrentEditor}
+              />);
+          }) : null
+        }
+      </React.Fragment> }
     </NavWrapper>
     </LeftNavWrapper>
   </LeftSidebar>
