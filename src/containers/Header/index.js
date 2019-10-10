14523b1c8c375546cd4ea8fb6c0cdada27b2a5d8
Emizel <Ernestogl5@hotmail.com>
2019-10-10 11:56:10 -0400

    feat (src): change the github ribbon for a hyperlink in top nav bar - 106
    Signed-off-by: Emizel <Ernestogl5@hotmail.com>

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -2,38 +2,68 @@ import React from 'react';
 import styled from 'styled-components';
 
 import image from '../../../public/img/logo.png';
+import logo from '../../../public/img/github.png';
 
 import { AP_THEME } from '../App/themeConstants';
 
 const HeaderWrapper = styled.div`
-    display: flex;
-    height: 37px;
-    width: 100%;
-    background-color: ${AP_THEME.DARK_BLUE};
-    color: white;
+  display: flex;
+  height: 37px;
+  width: 100%;
+  background-color: ${AP_THEME.DARK_BLUE};
+  color: white;
 `;
 
 const HeaderTitle = styled.div`
-    display: flex;
-    align-items: center;
-    margin-left: 30px;
-    padding-right: 20px;
-    text-align: center;
+  display: flex;
+  align-items: center;
+  margin-left: 30px;
+  padding-right: 20px;
+  text-align: center;
 `;
 
 const HeaderImage = styled.img`
-    height: 27px;
-    margin-top: 5px;
-    margin-left: 15px;
+  height: 27px;
+  margin-top: 5px;
+  margin-left: 15px;
 `;
 
+const HeaderLink = styled.a`
+  font-size: 17px;
+  color: #fff;
+  position: absolute;
+  top: 10px;
+  right: 12px;
+  transition: all 0.2s;
+
+  &:hover {
+    color: rgb(25, 198, 199);
+  }
+`;
+
+const HeaderLogo = styled.img`
+  position: absolute;
+  top: -6px;
+  right: 176px;
+  height: 28px;
+  transition: all 0.2s;
+`;
 const Header = () => (
-    <HeaderWrapper>
-            <a href="https://www.accordproject.org">
-                <HeaderImage src={image} alt="Accord Project" />
-            </a>
-        <HeaderTitle>Template Studio</HeaderTitle>
-    </HeaderWrapper>
+  <HeaderWrapper>
+    <a href="https://www.accordproject.org">
+      <HeaderImage src={image} alt="Accord Project" />
+    </a>
+
+    <HeaderTitle>Template Studio</HeaderTitle>
+
+    <HeaderLink
+      title="Please report issues and contribute improvements on GitHub!"
+      href="https://github.com/accordproject/template-studio-v2"
+      target="_blank"
+    >
+      <HeaderLogo src={logo} alt="Github logo" /> Contribute on Github
+    </HeaderLink>
+  </HeaderWrapper>
 );
 
 export default Header;
